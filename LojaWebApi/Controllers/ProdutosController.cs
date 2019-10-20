3e5e21eecde07e3d09d6af5e3ea94c0e23f27a2c
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-10-19 22:07:44 -0300

    initial commit

---
+++ LojaWebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,46 @@
+﻿using LojaWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace LojaWebApi.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        public List<Produto> Get()
+        {
+            var produtos = new List<Produto>
+            {
+                new Produto
+                {
+                    Id        = 1,
+                    Nome      = "Notebook Samsung",
+                    Foto      = "https://images-americanas.b2w.io/produtos/01/00/img12/134412/9/134412967_1GG.jpg",
+                    Descricao = "Notebook Samsung Expert X30 Intel Core I5 Quad-core 8GB 1TB Tela LED HD 15.6” Windows 10 Home - Cinza."
+                },
+                new Produto
+                {
+                    Id        = 2,
+                    Nome      = "Mcleod  Mueller",
+                    Foto      = "https://images-americanas.b2w.io/produtos/01/00/item/124651/3/124651374_1GG.jpg",
+                    Descricao = "Fogão Piso Consul 4 Bocas CFO4N Branco Bivolt."
+                },
+                new Produto
+                {
+                    Id        = 3,
+                    Nome      = "Aguirre  Ellis",
+                    Foto      = "https://www.idealmarketing.com.br/blog/wp-content/uploads/2018/02/produto.png",
+                    Descricao = "I am a very simple card. I am good at containing small bits of information."
+                },
+                new Produto
+                {
+                    Id        = 4,
+                    Nome      = "Cook  Tyson",
+                    Foto      = "https://www.idealmarketing.com.br/blog/wp-content/uploads/2018/02/produto.png",
+                    Descricao = "I am a very simple card. I am good at containing small bits of information."
+                }
+            };
+            return produtos;
+        }
+    }
+}
---
+++ LojaWebApi/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaWebApi.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Foto { get; set; }
+        public string Descricao { get; set; }
+    }
+}

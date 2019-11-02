5b43e4bc6b2c481dd511f7d9ed426dd3fde8f1b5
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-11-02 16:44:59 -0300

    login

---
+++ LojaWebApi/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using LojaWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaWebApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Login/Verificar")]
+        public bool Verificar(Login login) 
+        {
+            if (login == null) return false;
+            if (login.Email == "Max" && login.Senha == "123") return true;
+            return false;
+        }
+    }
+}
---
+++ LojaWebApi/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace LojaWebApi.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
